1caffd1d48f683446b2ea427b645a62d3179fece
qiancheng720 <1693914628@qq.com>
2019-08-27 23:51:26 +0800

    git commit -m

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,31 @@
 //配置路由相关信息
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Home from '../views/Home'
+import About from '../views/About'
 //通过Vue.use(插件),安装插件
 Vue.use(Router);
 
 //创建Router对象
 //映射关系
 const routes = [
-    
+    {
+        path:'',
+        redirect:'/home'
+    },
+    {
+        path:"/home",
+        component: Home
+    },
+    {
+        path:"/about",
+        component: About
+    }
+
 ]
 const router = new Router([
     //配置路由和组件之间的应用关系
-    routes
+    routes,
 ])
 
 //将router传到vue实例中
