400b07736ef305e5ff23c9dfa3eaaa630ec6a0b6
qiancheng720 <1693914628@qq.com>
2019-08-27 20:50:36 +0800

    测试git

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+//配置路由相关信息
+import Vue from 'vue'
+import Router from 'vue-router'
+
+//通过Vue.use(插件),安装插件
+Vue.use(Router);
+
+//创建Router对象
+//映射关系
+const routes = [
+    
+]
+const router = new Router([
+    //配置路由和组件之间的应用关系
+    routes
+])
+
+//将router传到vue实例中
+export default router
